d07ef1342ac08b65dd675666169bde252c780804
hfwang <hfwang1111@foxmail.com>
2020-10-20 14:10:42 +0800

    delete makefile

--- main.cpp
+++ main.cpp
@@ -12,11 +12,23 @@ using std::endl;
 using std::getline;
 
 
+int q4_21() {
+    vector<int> ivec{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    for (auto i : ivec) {
+        cout << ((i & 0x1) ? i * 2 : i) << " ";
+    }
+    cout << endl;
+    return 0;
+}
+
 
 
 
 int main() {
-    cout << "hello world!"<<endl;
+    q4_21();
+    cout << "hello world!" << endl;
     return 0;
+
 }
+
 
